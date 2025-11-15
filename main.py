e31d470b07fa90f6f64803bb34c5e9f7920126e9
Giacomo Scorcioni <giacomo.scorcioni@fermi.mo.it>
2025-11-15 09:01:37 +0100

    Aggiunta filtering publishers con id (sviluppo)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import tornado.web
 import asyncio
+
+from bson import ObjectId
 from pymongo import AsyncMongoClient
 
 client = AsyncMongoClient("localhost", 27017)
@@ -9,11 +11,27 @@ publishers=db['publishers']
 books= db['books']
 
 class Main(tornado.web.RequestHandler):
+    async def get(self, id=None):
+        name = self.get_query_argument("name", None)
+        country=self.get_query_argument("country", None)
+
+
+        if id:
+            filtered_publisher = await publishers.find({"_id": id })
+        else:
+            if name and country:
+                filtered_publisher= await publishers.find({"$and": [{"name": name},{"country": country}]})
+            elif name:
+                filtered_publisher = await publishers.find({"name": name})
+            elif country:
+                filtered_publisher = await publishers.find({"country": country})
+            else:
+                filtered_publisher= await publishers.find()
+
+
     def post(self):
         pass
 
-    def get(self):
-        pass
 
 def make_app():
        return tornado.web.Application([])
