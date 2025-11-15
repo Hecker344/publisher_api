536985c4edd5b17d871b5b1df38bd812501203df
Giacomo Scorcioni <giacomo.scorcioni@fermi.mo.it>
2025-11-15 08:26:19 +0100

    main base tornado

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import tornado.web
+import asyncio
+from pymongo import AsyncMongoClient
+
+class Main(tornado.web.RequestHandler):
+    def post(self):
+        pass
+
+    def get(self):
+        pass
+
+def make_app():
+       return tornado.web.Application([])
+
+async def main(shutdown_event):
+    app = make_app()
+    app.listen(8888)
+    print("Server attivo su http://localhost:8888/main")
+    await shutdown_event.wait()
+    print("Chiusura server...")
+
+
+if __name__ == "__main__":
+    shutdown_event = asyncio.Event()
+    try:
+        asyncio.run(main(shutdown_event))
+    except KeyboardInterrupt:
+        shutdown_event.set()
